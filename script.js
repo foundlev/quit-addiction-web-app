5e766da23c71a2e204ed5552de30a93d114dc82e
Lev <168222654+foundlev@users.noreply.github.com>
2025-05-31 20:42:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const startDate = new Date(Date.UTC(2025, 4, 25, 12, 38, 0));
+    const startDate = new Date(Date.UTC(2025, 4, 31, 11, 46, 0));
     const today = new Date();
     const daysPassedFloat = (today.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
     const daysPassed = Math.floor(daysPassedFloat);
